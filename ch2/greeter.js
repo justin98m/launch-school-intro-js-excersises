4e69570ddf598c6d91922d42c51d78654044915e
Justin Cameron <justin1998m1@gmail.com>
2022-04-19 23:37:40 -0400

    Ch2 excersises completed (Variables)

---
+++ ch2/greeter.js
@@ -0,0 +1,8 @@
+//Write a program tha greets the name victor 3 times
+var v = " Victor"
+var greet = "greetings"
+var hello = "hello"
+var goodbye = "goodbye"
+console.log(greet,v);
+console.log(hello,v);
+console.log(goodbye,v);
